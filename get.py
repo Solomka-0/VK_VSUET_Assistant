605eed579d2d3a709a60b719c1a3dcfbf62ae031
Solomka-0 <krasjuk2@gmail.com>
2021-02-26 12:14:49 +0300

    Исправлена ошибка при скачивании файла с источника

--- get.py
+++ get.py
@@ -6,27 +6,35 @@ file_names = {'uits':'timetables/uits.xlsx', 'pma':'timetables/pma.xlsx', 'eht':
 def get():
     url_name = 'https://vsuet.ru'
     url_path = '/student/schedule'
-    page = requests.post(url_name + url_path)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    uls = soup.find_all('ul')
+    try:
+        page = requests.post(url_name + url_path)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        uls = soup.find_all('ul')
 
-    references = []
-    for ul in uls:
-        if 'i-file' in ul['class']:
-            if ul.a['href'].find('.xlsx') != -1:
-                references.append(ul.a['href'])
+        references = []
+        for ul in uls:
+            if 'i-file' in ul['class']:
+                if ul.a['href'].find('.xlsx') != -1:
+                    references.append(ul.a['href'])
 
-    for i in range(0, len(references)):
-        references[i] = url_name + references[i]
+        for i in range(0, len(references)):
+            references[i] = url_name + references[i]
 
-    for file_name in file_names.keys():
-        file = open(file_names[file_name], 'wb')
-        for reference in references:
-            if file_name in reference:
-                print(f'Загружен {file_names[file_name]}')
-                ufr = requests.get(reference)
-                file.write(ufr.content)
-                file.close()
-                break
+        for file_name in file_names.keys():
+            file = open(file_names[file_name], 'wb')
+            for reference in references:
+                if file_name in reference:
+                    print(f'Загружен {file_names[file_name]}')
+                    ufr = requests.get(reference)
+                    file.write(ufr.content)
+                    file.close()
+                    break
+        return True
+    except:
+        for file_name in file_names.keys():
+            file = open(file_names[file_name], 'a')
+            print(f'{file_names[file_name]} не был загружен')
+            file.close()
+        return False
 
 get()
